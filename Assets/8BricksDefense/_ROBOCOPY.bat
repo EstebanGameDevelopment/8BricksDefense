6a95aa1cd7d710d0cf4e7f3d5bd3691000982f8d
Esteban <services@yourvrexperience.com>
2018-03-24 14:25:13 +0100

    Removed empty scene

--- Assets/8BricksDefense/_ROBOCOPY.bat
+++ Assets/8BricksDefense/_ROBOCOPY.bat
@@ -1,2 +1,2 @@
-robocopy C:\Users\Esteban\Desktop\YourNetworkTools\8BricksDefense\Assets\8BricksDefense C:\Users\Esteban\Desktop\YourNetworkTools\8BricksDefense\_CLIENTS\8BricksDefenseClient1\Assets\8BricksDefense /MIR
+robocopy C:\Users\Esteban\Desktop\GitHub\8BricksDefense\Assets\8BricksDefense C:\Users\Esteban\Desktop\GitHub\8BricksDefense\__CLIENTS\8Bricks_Client1\Assets\8BricksDefense /MIR
 pause
