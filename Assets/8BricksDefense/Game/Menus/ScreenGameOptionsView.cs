5b202cae18db3d02485222cd47c4e6e3bd362b7b
Esteban <services@yourvrexperience.com>
2018-06-01 10:41:05 +0200

    Updated screens of the Network plugin in order to work with the updated Screen Controller.

--- Assets/8BricksDefense/Game/Menus/ScreenGameOptionsView.cs
+++ Assets/8BricksDefense/Game/Menus/ScreenGameOptionsView.cs
@@ -8,103 +8,106 @@ using UnityEngine.UI;
 using YourCommonTools;
 using YourNetworkingTools;
 
-/******************************************
-* 
-* ScreenGameOptionsView
-* 
-* We will use this screen to set the game options
-* 
-* @author Esteban Gallardo
-*/
-public class ScreenGameOptionsView : ScreenBaseView, IBasicView
+namespace EightBricksDefense
 {
-	public const string SCREEN_NAME = "SCREEN_GAME_OPTIONS";
-
-	// ----------------------------------------------
-	// PRIVATE MEMBERS
-	// ----------------------------------------------	
-	private GameObject m_root;
-	private Transform m_container;
-
-	// -------------------------------------------
-	/* 
-		* Constructor
-		*/
-	public override void Initialize(params object[] _list)
-	{
-		base.Initialize(_list);
-
-		m_root = this.gameObject;
-		m_container = m_root.transform.Find("Content");
-
-		m_container.Find("Title").GetComponent<Text>().text = LanguageController.Instance.GetText("message.game.title");
-
-		GameObject playInVRGame = m_container.Find("Button_EnableVR").gameObject;
-		playInVRGame.transform.Find("Text").GetComponent<Text>().text = LanguageController.Instance.GetText("screen.play.as.vr.game");
-		playInVRGame.GetComponent<Button>().onClick.AddListener(PlayInVRPressed);
-
-		GameObject playWithGyroscopeGame = m_container.Find("Button_EnableGyroscope").gameObject;
-		playWithGyroscopeGame.transform.Find("Text").GetComponent<Text>().text = LanguageController.Instance.GetText("screen.play.with.gyroscope");
-		playWithGyroscopeGame.GetComponent<Button>().onClick.AddListener(PlayWithGyroscopePressed);
-
-		UIEventController.Instance.UIEvent += new UIEventHandler(OnMenuEvent);
-	}
-
-	// -------------------------------------------
-	/* 
-		* GetGameObject
-		*/
-	public GameObject GetGameObject()
-	{
-		return this.gameObject;
-	}
-
-	// -------------------------------------------
-	/* 
-	* Destroy
+	/******************************************
+	* 
+	* ScreenGameOptionsView
+	* 
+	* We will use this screen to set the game options
+	* 
+	* @author Esteban Gallardo
 	*/
-	public override bool Destroy()
+	public class ScreenGameOptionsView : ScreenBaseView, IBasicView
 	{
-		if (base.Destroy()) return true;
+		public const string SCREEN_NAME = "SCREEN_GAME_OPTIONS";
 
-		UIEventController.Instance.UIEvent -= OnMenuEvent;
-		GameObject.Destroy(this.gameObject);
+		// ----------------------------------------------
+		// PRIVATE MEMBERS
+		// ----------------------------------------------	
+		private GameObject m_root;
+		private Transform m_container;
 
-		return false;
-	}
+		// -------------------------------------------
+		/* 
+			* Constructor
+			*/
+		public override void Initialize(params object[] _list)
+		{
+			base.Initialize(_list);
 
-	// -------------------------------------------
-	/* 
-		* PlayInVRPressed
+			m_root = this.gameObject;
+			m_container = m_root.transform.Find("Content");
+
+			m_container.Find("Title").GetComponent<Text>().text = LanguageController.Instance.GetText("message.game.title");
+
+			GameObject playInVRGame = m_container.Find("Button_EnableVR").gameObject;
+			playInVRGame.transform.Find("Text").GetComponent<Text>().text = LanguageController.Instance.GetText("screen.play.as.vr.game");
+			playInVRGame.GetComponent<Button>().onClick.AddListener(PlayInVRPressed);
+
+			GameObject playWithGyroscopeGame = m_container.Find("Button_EnableGyroscope").gameObject;
+			playWithGyroscopeGame.transform.Find("Text").GetComponent<Text>().text = LanguageController.Instance.GetText("screen.play.with.gyroscope");
+			playWithGyroscopeGame.GetComponent<Button>().onClick.AddListener(PlayWithGyroscopePressed);
+
+			UIEventController.Instance.UIEvent += new UIEventHandler(OnMenuEvent);
+		}
+
+		// -------------------------------------------
+		/* 
+			* GetGameObject
+			*/
+		public GameObject GetGameObject()
+		{
+			return this.gameObject;
+		}
+
+		// -------------------------------------------
+		/* 
+		* Destroy
 		*/
-	private void PlayInVRPressed()
-	{
-		SoundsController.Instance.PlaySingleSound(SoundsConfiguration.SOUND_SELECTION_FX);
-		CardboardLoaderVR.SaveEnableCardboard(true);
-		MenuScreenController.Instance.CreateOrJoinRoomInServer(false);
-		Destroy();
-		MenuScreenController.Instance.CreateNewScreen(ScreenLoadingView.SCREEN_NAME, UIScreenTypePreviousAction.KEEP_CURRENT_SCREEN, false, null);		
-	}
+		public override bool Destroy()
+		{
+			if (base.Destroy()) return true;
 
-	// -------------------------------------------
-	/* 
-	* JoinGamePressed
-	*/
-	private void PlayWithGyroscopePressed()
-	{
-		SoundsController.Instance.PlaySingleSound(SoundsConfiguration.SOUND_SELECTION_FX);
-		CardboardLoaderVR.SaveEnableCardboard(false);
-		MenuScreenController.Instance.CreateOrJoinRoomInServer(false);
-		Destroy();
-		MenuScreenController.Instance.CreateNewScreen(ScreenLoadingView.SCREEN_NAME, UIScreenTypePreviousAction.KEEP_CURRENT_SCREEN, false, null);
-	}
+			UIEventController.Instance.UIEvent -= OnMenuEvent;
+			UIEventController.Instance.DispatchUIEvent(UIEventController.EVENT_SCREENMANAGER_DESTROY_SCREEN, this.gameObject);
 
-	// -------------------------------------------
-	/* 
-	* OnMenuBasicEvent
-	*/
-	protected override void OnMenuEvent(string _nameEvent, params object[] _list)
-	{
-		base.OnMenuEvent(_nameEvent, _list);
+			return false;
+		}
+
+		// -------------------------------------------
+		/* 
+			* PlayInVRPressed
+			*/
+		private void PlayInVRPressed()
+		{
+			SoundsController.Instance.PlaySingleSound(SoundsConfiguration.SOUND_SELECTION_FX);
+			CardboardLoaderVR.SaveEnableCardboard(true);
+			MenuScreenController.Instance.CreateOrJoinRoomInServer(false);
+			Destroy();
+			MenuScreenController.Instance.CreateNewScreen(ScreenLoadingView.SCREEN_NAME, UIScreenTypePreviousAction.KEEP_CURRENT_SCREEN, false, null);
+		}
+
+		// -------------------------------------------
+		/* 
+		* JoinGamePressed
+		*/
+		private void PlayWithGyroscopePressed()
+		{
+			SoundsController.Instance.PlaySingleSound(SoundsConfiguration.SOUND_SELECTION_FX);
+			CardboardLoaderVR.SaveEnableCardboard(false);
+			MenuScreenController.Instance.CreateOrJoinRoomInServer(false);
+			Destroy();
+			MenuScreenController.Instance.CreateNewScreen(ScreenLoadingView.SCREEN_NAME, UIScreenTypePreviousAction.KEEP_CURRENT_SCREEN, false, null);
+		}
+
+		// -------------------------------------------
+		/* 
+		* OnMenuBasicEvent
+		*/
+		protected override void OnMenuEvent(string _nameEvent, params object[] _list)
+		{
+			base.OnMenuEvent(_nameEvent, _list);
+		}
 	}
 }
